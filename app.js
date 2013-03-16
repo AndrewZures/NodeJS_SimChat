ff2ffa7e3dabb9b6dc3359f1db65d3a8cead437e
Andrew James Zures <andrew.zures@gmail.com>
2013-03-16 18:45:07 -0500

    final udpates

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ io.configure(function () {
   	io.set("polling duration", 10); 
 });
 
+
 //app will listen on port specified above
 app.listen(port);
 
@@ -22,63 +23,66 @@ app.get('/', function (req, res) {
 });
 
 // usernames which are currently connected to the chat
-var usernames = {};
-var usersockets= {};
+var usernames = {}; //stores usernames
+var usersockets= {};  //stores user sockets
 
 //on receiving socket "connection, anonymous function called"
 io.sockets.on('connection', function (socket) {
-
-	// when the client emits 'sendchat', this listens and executes
-	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-		//first parameter is the socket emiiting, second is the data itself
-		io.sockets.emit('updatechat', socket.username, data);
-
-	});
 
 	//socketname getter
 	socket.on('getusername', function(){
 		socket.emit('sendusername', socket.username);
 	});
 
+	// when the client emits 'sendchat', this listens and executes
+	socket.on('sendchat', function (chat) {
+		// we tell the client to execute 'updategroupchat' with 2 parameters
+		//first parameter is the socket emiiting, second is the data itself
+		io.sockets.emit('updategroupchat', socket.username, chat);
+
+	});
+
 	
-	socket.on('privatechat', function (data, RSname) {
-		Receiver = usersockets[RSname];
-		if (Receiver != null){
-			recName = Receiver.username;
-			Receiver.emit('myprivatechat', socket.username, recName, data);
-			//socket.emit('myprivatechat', socket.username, 'You', data);
+	socket.on('privatechat', function (data, target) {
+		targetSocket = usersockets[target]; //get user socket
+
+		//determine if user socket exists
+		if (targetSocket != null){
+
+			//if user socket exists, get the socket's username
+			confirmedTarget = TargetSocket.username;
+
+			//emit chat to target socket with sender's name, target's name, and data
+			targetSocket.emit('myprivatechat', socket.username, confirmedTarget, data);
 		}
 	});
 
 	// when the client emits 'adduser', this listens and executes
 	socket.on('adduser', function(username){
-		// we store the username in the socket session for this client
-		socket.username =  username;
-		// add the client's username to the global list
-		usernames[username] = username;
-		// add socket to usersockets
-		usersockets[socket.username] = socket;
-		// echo to client they've connected
-		// just emitting to "local" socket
-		socket.emit('updatechat', 'SERVER', 'you have connected');
-		// echo globally (all clients) that a person has connected
-		//emitting to everything but local socket
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has connected');
-		// update the list of users in chat, client-side
-		socket.emit('updatename', socket.username);
-		//update all
+		socket.username =  username;  //assign username to socket
+		usernames[username] = username;  //store username is username hash
+		usersockets[socket.username] = socket; //store socket is socket.hash
+
+		//send to connected client that they have connected
+		socket.emit('updateserverchat', 'SERVER', 'you have connected');
+
+		//broadcast new user to all current sockets
+		socket.broadcast.emit('updateserverchat', 'SERVER', socket.username + ' has connected');
+
+		//update all users newly changed user list
 		io.sockets.emit('updateusers', usernames);
 	});
 
 	// when the user disconnects.. perform this
 	socket.on('disconnect', function(){
-		// remove the username from global usernames list
-		delete usernames[socket.username];
-		// update list of users in chat, client-side
+		delete usernames[socket.username]; //remove username from username array
+		delete usersockets[socket.usernae]; //remove socket from sockets array
+
+		//update all users with newly updated user list
 		io.sockets.emit('updateusers', usernames);
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+
+		// echo globally that user has left on server chat
+		socket.broadcast.emit('updateserverchat', 'SERVER', socket.username + ' has disconnected');
 	});
 
 });
--- style.js
+++
@@ -1,9 +0,0 @@
-function createChatBox() {
-      var privDiv = document.getElementById(PrivateTab);
-      var privBox = document.createElement('div');
-      $(privBox).attr({
-          'id': target,
-          'style': 'float: right; width: 220px; height: 200px; overflow-x: auto; border-radius: 6px; border: 1px #BBB solid;'
-        });
-      PrivateTab.appendChild(privBox);
-}
